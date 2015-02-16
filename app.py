03dd9b829fd2ebb8d26bf9d9a4a77660cc54ca2e
Chris McDowall <chris.mcdowall@gmail.com>
2015-02-16 14:21:36 +1300

    First commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from random import randint
+
+from flask import Flask
+from flask.ext import restful
+
+app = Flask(__name__)
+api = restful.Api(app)
+
+
+class DiceSingle(restful.Resource):
+    def get(self, num_sides):
+        results = [ randint(1, num_sides) ]
+        return {
+            'number_of_rolls': 1,
+            'number_of_sides': num_sides,
+            'rolls': results,
+            'total': sum(results),
+        }
+
+
+class DiceMulti(restful.Resource):
+    def get(self, num_sides, num_rolls):
+        results = [ randint(1, num_sides) for i in range(num_rolls) ]
+        return {
+            'number_of_rolls': 1,
+            'number_of_sides': num_sides,
+            'rolls': results,
+            'total': sum(results),
+        }
+
+api.add_resource(DiceSingle, '/dice/<int:num_sides>')
+api.add_resource(DiceMulti, '/dice/<int:num_sides>/<int:num_rolls>')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
